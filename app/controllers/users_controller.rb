116a8a3aa5aab8b00349f4cd18ffdb1f8ee7f83a
beranemo <beranemo@gmail.com>
2018-05-31 23:43:26 +0800

    updates sign up function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,14 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def sign_up_submit
+    @user = User.new(user_params)
+    @user.save
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit(:name, :password)
+  end
+  
 end
