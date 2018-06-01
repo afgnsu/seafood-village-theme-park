f4af8cb78783a4518c7dbacdbe664c73046987cb
beranemo <beranemo@gmail.com>
2018-06-01 20:46:06 +0800

    refatcor signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,14 @@ class UsersController < ApplicationController
   def sign_up
     @user = User.new(user_params)
     @user.save
+    puts "==================="
+    puts @user
     redirect_to root_path
   end
   
   private
   def user_params
-    params.require(:user).permit(:name, :password)
+    params.require(:user).permit(:name)
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file
   # see http://guides.rubyonrails.org/routing.html
-  get "/park/sign_up", to: "users#sign_up_new"
-  post "/park", to: "users#sign_up"
+  get  "/signup", to: "users#sign_up_new"
+  post "/signup", to: "users#sign_up"
   
   root 'attractions#index'
   resources :attractions, only: [:index]
