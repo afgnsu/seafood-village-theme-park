826923e475e1fb819b3ec5fde7f8e3c1608aaf86
beranemo <beranemo@gmail.com>
2018-06-02 18:22:54 +0800

    add flash alert

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
     if user and user.authenticate(params[:session][:password])
       redirect_to root_path
     else
+      flash.now[:alert] = "喔喔，某些錯誤發生了"
       render "new"
     end
   end
