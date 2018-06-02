2cc61e959d631ad86aa10314e9a0e2bfa2c37d5d
beranemo <beranemo@gmail.com>
2018-06-02 22:40:29 +0800

    complete current_usre function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:session][:name].downcase)
     puts user
     if user and user.authenticate(params[:session][:password])
+      log_in(user)
       redirect_to root_path
     else
       flash.now[:alert] = "喔喔，某些錯誤發生了"
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,10 @@
 module SessionsHelper
   
+  # 使用者登入後，把使用者的 id 存到 session 裡 
+  def log_in(user)
+    session[:user_id] = user.id 
+  end
+  
   # 如果有 session 裡有值，就把它取出來
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
