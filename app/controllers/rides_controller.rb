5ae1b44c17af4525c9f29325fbacde7d25228a2f
beranemo <beranemo@gmail.com>
2018-06-03 03:35:34 +0800

    remvoe unused code

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -8,12 +8,4 @@ class RidesController < ApplicationController
     redirect_to root_path
   end
   
-  def create
-    @ride = Ride.create(
-      user: current_user,
-      attraction_id: params[:attraction_id]))
-  
-    redirect_to root_path
-  end
-  
 end
