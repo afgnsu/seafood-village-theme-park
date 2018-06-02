f4f1176d4124ee994f7be22db9e14157fd4060a3
beranemo <beranemo@gmail.com>
2018-06-02 17:38:09 +0800

    completet the login page

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
   root 'attractions#index'
   resources :attractions, only: [:index]
   
-  resource :sessions, only: [:new, :create, :destroy]
+  # resource :sessions, only: [:new, :create, :destroy]
+  get    "/login",  to: "sessions#new"
+  post   "/login",  to: "sessions#destroy"
+  delete "/logout", to: "sessions#destroy"
 end
